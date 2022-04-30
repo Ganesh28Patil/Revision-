4d61a962c793737e16660378eb9dd3ff444f3bd0
Ganesh Patil <80849916+Ganesh28Patil@users.noreply.github.com>
2022-04-30 19:15:08 +0530

    check

---
+++ dsa_subStrring.js
@@ -0,0 +1,19 @@
+// generate sub-String 
+let testCase = 'abcde';
+let arr = [];
+function generateSubString(str) {
+        for (let k = 0; k < str.length; k++) {
+                for (var i = 0; i < str.length; i++) {
+                        let res = "";                        
+                        for (var j = k; j < str.length - i; j++) {
+                                res += str[j];
+                        }
+                        console.log(res);
+                        // arr.push(res);
+                }
+        }
+
+}
+generateSubString(testCase);
+
+console.log(arr);
